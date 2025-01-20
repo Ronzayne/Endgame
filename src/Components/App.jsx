84ca497a3c480074e3ff478d1183e60b0f203593
Ronzayne <ronzayne7@gmail.com>
2025-01-20 11:35:40 +0000

    Wrong guess and Game Over functionality added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { clsx } from "clsx";
 import { languages } from "../data/languages";
 
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react");
   const [guessedLetters, setGuessedLetters] = useState([]);
-  console.log(guessedLetters);
 
   const letters = "abcdefghijklmnopqrstuvwxyz";
 
+  const wrongGuessCount = guessedLetters.filter(
+    (letter) => !currentWord.includes(letter)
+  ).length;
+
+  const isGameWon = currentWord
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+
+  // console.log(isGameWon);
+  const isGameLost = wrongGuessCount >= languages.length - 1;
+
+  const isGameOver = isGameWon || isGameLost;
+  console.log(isGameOver);
   function getGuessedLetters(letter) {
     setGuessedLetters((prevVal) =>
       prevVal.includes(letter) ? prevVal : [...prevVal, letter]
     );
   }
-  const languageElement = languages.map((el) => {
+
+  console.log(wrongGuessCount);
+  const languageElement = languages.map((el, index) => {
+    const isLangLost = index < wrongGuessCount;
+    const className = clsx(isLangLost && "lost");
     return (
       <span
+        className={className}
         key={el.name}
         style={{
           color: el.color,
@@ -40,7 +57,7 @@ export default function AssemblyEndgame() {
     const isGuessed = guessedLetters.includes(letters);
     const isCorrect = isGuessed && currentWord.includes(letters);
     const isWrong = isGuessed && !currentWord.includes(letters);
-
+    isWrong ? wrongGuessCount + 1 : null;
     const className = clsx({
       correct: isCorrect,
       wrong: isWrong,
@@ -78,7 +95,9 @@ export default function AssemblyEndgame() {
 
       <section className="keyboard">{keyBordElements}</section>
       <div className="new-game">
-        <button className="new-game-button">New Game</button>
+        {isGameOver ? (
+          <button className="new-game-button">New Game</button>
+        ) : null}
       </div>
     </main>
   );
